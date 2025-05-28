e00b6e7ba1066d81e219554f8d94c9eacfb3ef6b
Renan Queiroz <renanqueiroz92@gmail.com>
2025-05-28 19:28:38 -0300

    alteração da aplicação

--- cadastro cliente/src/dao/IClienteDAO.java
+++ cadastro cliente/src/dao/IClienteDAO.java
@@ -1,12 +1,16 @@
 package dao;
 
+import java.util.Collection;
+
 import model.Cliente;
-import java.util.List;
+
 
 public interface IClienteDAO {
-    void cadastrar(Cliente cliente);
-    Cliente buscarPorCpf(String cpf);
-    List<Cliente> listarTodos();
-    void atualizar(Cliente cliente);
-    void deletar(String cpf);
+
+    public Boolean cadastrar(CLiente cliente);
+    public void excluir (Long cpf);
+    public void alterar (Cliente cliente);
+    public Cliente consultar (Long cpf);
+    public Collection<Cliente> buscarTodos();
+    
 }
